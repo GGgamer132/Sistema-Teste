0138da5868df9b2e79dea002846362da58454e3f
Otavio Henrique <otavio.medeiros@tic.ufrj.br>
2026-09-12 21:51:17 -0300

    Configuracoes do Spring Boot e React + Vite + Tailwind

---
+++ backend/circula-book/src/main/java/com/circulabook/CirculaBookApplication.java
@@ -0,0 +1,13 @@
+package com.circulabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirculaBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirculaBookApplication.class, args);
+	}
+
+}
